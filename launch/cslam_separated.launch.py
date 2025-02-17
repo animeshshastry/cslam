2a712260bbca0e3fed04c1a02c02a188b5809289
animeshshastry <animeshs@umd.edu>
2025-02-17 16:19:21 -0500

    Local sync

--- launch/cslam_separated.launch.py
+++ launch/cslam_separated.launch.py
@@ -10,40 +10,44 @@ from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterFile
 
 def launch_setup(context, *args, **kwargs):
+    profile = LaunchConfiguration('profile').perform(context).lower() == 'true'
     loop_detection_node = Node(package='cslam',
                                executable='loop_closure_detection_node.py',
                                name='cslam_loop_closure_detection',
                                parameters=[
-                                   ParameterFile(LaunchConfiguration('config').perform(context), allow_substs=True), {
+                                   ParameterFile(LaunchConfiguration('base_params').perform(context), allow_substs=True),
+                                   ParameterFile(LaunchConfiguration('robot_params').perform(context), allow_substs=True), {
                                        "robot_id": LaunchConfiguration('robot_id'),
                                        "max_nb_robots": LaunchConfiguration('max_nb_robots'),
                                        "tf_prefix": LaunchConfiguration('tf_prefix'),
                                    }
                                ],
                                #prefix=['stdbuf -o L'],
-                               arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO'],
+                               arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO', '--log-level','rmw_zenoh_cpp:=FATAL'],
                                output='screen',
                                namespace=LaunchConfiguration('namespace'))
 
     map_manager_node = Node(package='cslam',
-                            executable='map_manager',
+                            executable='map_profiler' if profile else 'map_manager',
                             name='cslam_map_manager',
                             parameters=[
-                                ParameterFile(LaunchConfiguration('config').perform(context), allow_substs=True), {
+                                ParameterFile(LaunchConfiguration('base_params').perform(context), allow_substs=True),
+                                ParameterFile(LaunchConfiguration('robot_params').perform(context), allow_substs=True),  {
                                     "robot_id": LaunchConfiguration('robot_id'),
                                     "max_nb_robots": LaunchConfiguration('max_nb_robots'),
                                     "tf_prefix": LaunchConfiguration('tf_prefix'),
                                 }
                             ],
                             output='screen',
-                            arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO'],
+                            arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO', '--log-level','rmw_zenoh_cpp:=FATAL'],
                             namespace=LaunchConfiguration('namespace'))
 
     pose_graph_manager_node = Node(package='cslam',
                                    executable='pose_graph_manager',
                                    name='cslam_pose_graph_manager',
                                    parameters=[
-                                       ParameterFile(LaunchConfiguration('config').perform(context), allow_substs=True), {
+                                       ParameterFile(LaunchConfiguration('base_params').perform(context), allow_substs=True),
+                                        ParameterFile(LaunchConfiguration('robot_params').perform(context), allow_substs=True),  {
                                            "robot_id": LaunchConfiguration('robot_id'),
                                            "max_nb_robots": LaunchConfiguration('max_nb_robots'),
                                            "evaluation.enable_simulated_rendezvous": LaunchConfiguration('enable_simulated_rendezvous'),
@@ -52,7 +56,7 @@ def launch_setup(context, *args, **kwargs):
                                        }
                                    ],
                                    output='screen',
-                                   arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO'],
+                                   arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO', '--log-level','rmw_zenoh_cpp:=FATAL'],
                                    prefix="",#"xterm -e gdb -ex run --args", #LaunchConfiguration('launch_prefix_cslam'),# "gdbserver localhost:3000", # xterm -e gdb -ex run --args
                                    namespace=LaunchConfiguration('namespace'))
 
@@ -61,14 +65,15 @@ def launch_setup(context, *args, **kwargs):
                                 executable='global_descriptor',
                                 namespace=LaunchConfiguration('namespace'),
                                 parameters=[
-                                ParameterFile(LaunchConfiguration('config').perform(context), allow_substs=True), {
+                                ParameterFile(LaunchConfiguration('base_params').perform(context), allow_substs=True),
+                                ParameterFile(LaunchConfiguration('robot_params').perform(context), allow_substs=True),  {
                                         "robot_id": LaunchConfiguration('robot_id'),
                                         "max_nb_robots": LaunchConfiguration('max_nb_robots'),
                                         "evaluation.enable_simulated_rendezvous": LaunchConfiguration('enable_simulated_rendezvous'),
                                         "evaluation.rendezvous_schedule_file": LaunchConfiguration('rendezvous_schedule_file'),
                                     }
                                 ],
-                                arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO'],
+                                arguments=['--ros-args','--log-level',LaunchConfiguration('log_level'),'--log-level','rcl:=INFO', '--log-level','rmw_zenoh_cpp:=FATAL'],
                                output='screen',
                                #prefix="pprofile -o cslam.pprofile",
                             )
@@ -88,14 +93,22 @@ def generate_launch_description():
         DeclareLaunchArgument('robot_id', default_value='0', description=''),
         DeclareLaunchArgument('tf_prefix', default_value=PythonExpression(['("', LaunchConfiguration('namespace'), '".strip("/") + "/").lstrip("/")'])),
         DeclareLaunchArgument('max_nb_robots', default_value='2', description=''),
+        DeclareLaunchArgument('profile', default_value='false', description=''),
         DeclareLaunchArgument('config_path', default_value='/config/', description=''),
-        DeclareLaunchArgument('config_file', default_value='cslam_hl2_stereo.yaml', description=''),
-        DeclareLaunchArgument('config',
+        DeclareLaunchArgument('base_config', default_value='cslam_shared.yaml', description=''),
+        DeclareLaunchArgument('robot_config', default_value='hl2_stereo.yaml', description=''),
+        DeclareLaunchArgument('base_params',
                               default_value=[
                                   LaunchConfiguration('config_path'),
-                                  LaunchConfiguration('config_file')
+                                  LaunchConfiguration('base_config')
                               ],
                               description=''),
+        DeclareLaunchArgument('robot_params',
+                        default_value=[
+                            LaunchConfiguration('config_path'),
+                            LaunchConfiguration('robot_config')
+                        ],
+                        description=''),
         DeclareLaunchArgument(
             'launch_prefix_cslam',
             default_value='',
